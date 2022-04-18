ee86816a1d7ab040d0da69b9bf6df414a8803b12
astraughn5 <astraughn5@gmail.com>
2022-04-18 17:40:14 -0500

    adding codewars, solved the solution

--- 8kyu/how good are you/main.js
+++ 8kyu/how good are you/main.js
@@ -1,1 +1,76 @@
+function betterThanAverage(classPoints, yourPoints) {
+	let sum = 0
+	let students = 0
+	let average = 0
+	classPoints.forEach((element,index) => {
+	  sum += element
+	  students = index + 1
+	  average = sum / students
+	})
+	
+	// console.log(average)
+	// console.log(yourPoints)
 
+	if (yourPoints > average){
+		return true
+	}
+	else{
+		return false
+	}
+
+
+	// console.log(sum)
+	// console.log(students)
+	
+  }
+  
+console.log(betterThanAverage([2,4,4],6))
+
+
+
+/* attempt 2 trying to use turnary 
+function betterThanAverage(classPoints, yourPoints) {
+	let sum = 0
+	let students = 0
+	let average = 0
+	classPoints.forEach((element,index) => {
+	  sum += element
+	  students = index + 1
+	  average = sum / students
+	})
+	
+	console.log(average)
+	console.log(yourPoints)
+
+	yourPoints > average ?  console.log(true) : console.log(false)
+
+	// console.log(sum)
+	// console.log(students)
+	
+  }
+  
+  best practice solution 1
+  function betterThanAverage(classPoints, yourPoints) {
+	return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length; 
+  }
+
+  best practice solution 2
+  function betterThanAverage(classPoints, yourPoints) {
+  // Your code here
+  var classAvg = 0;
+  for (var i = 0; i < classPoints.length; i++){
+    classAvg += classPoints[i]; 
+  }
+  classAvg = classAvg/classPoints.length; 
+  return yourPoints > classAvg;
+}
+
+  best practice solution 3
+
+function betterThanAverage(classPoints, yourPoints) {
+  const allPoints = [yourPoints, ...classPoints];
+  const average = allPoints.reduce((acc,val) => acc + val) / allPoints.length;
+  
+  return yourPoints > average; 
+}
+*/
