c1f0f937e8fa129f49c863798ee7c05a2a16aae7
astraughn5 <astraughn5@gmail.com>
2022-04-20 13:40:03 -0500

    adding daily codewars, I solved this code

--- 8kyu/Name Shuffler/main.js
+++ 8kyu/Name Shuffler/main.js
@@ -1,1 +1,23 @@
+// Write a function that returns a string in which firstname is swapped with last name.
 
+// nameShuffler('john McClane'); => "McClane john"
+// my solution
+function nameShuffler(str){
+	const split = str.split(' ')
+	console.log(split)
+	console.log(split[1]+ ' ' + split[0])
+}
+
+nameShuffler('Alex Straughn')
+
+//Best solution
+function nameShuffle(str){
+	console.log(str.split(' ').reverse().join(' '))
+  }
+  nameShuffle('Alex Straughn')
+
+
+
+// split(' ') takes the string and splits each word by the space and adds it to a separate index in an array
+// reverce() takes the array and reverses the index
+// join(' ') takes the array and joins the index by the space
