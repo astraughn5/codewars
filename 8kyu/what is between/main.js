727d2af64e7051002db98c9492bd75d3866f1b6d
astraughn5 <astraughn5@gmail.com>
2022-04-21 09:48:35 -0500

    updating

--- 8kyu/what is between/main.js
+++ 8kyu/what is between/main.js
@@ -5,6 +5,12 @@
 // a = 1
 // b = 4
 // --> [1, 2, 3, 4]
+//PREP
+//P: integers, whole, could be + or -
+//R: return array including a & b
+//E:
 function between(a, b) {
 	// your code here
   }
+
+between(1,4) //output [1,2,3,4]
