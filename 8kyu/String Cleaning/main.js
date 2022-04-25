e42dd7799a85c1ce7f82d8774fd951a8dfc58be0
astraughn5 <astraughn5@gmail.com>
2022-04-25 16:33:48 -0500

    solved solution by researching replace method and reg ex, forgot to return and it took more research to fix undefined error

--- 8kyu/String Cleaning/main.js
+++ 8kyu/String Cleaning/main.js
@@ -1,1 +1,16 @@
+function stringClean(s){
+	// Function will return the cleaned string
+	newS = s.replace(/[0123456789]/g,'')
+	return newS
+  }
 
+  console.log(stringClean('123gt!aa ss'))
+
+  //best solutions
+  function stringClean(s){
+	return s.replace(/\d/g, "");
+  }
+
+  function stringClean(s){
+	return s.replace(/[0-9]/g, '');
+  }
