0988b29d248c889700c2c0dfc33a07d4957f1d1f
astraughn5 <astraughn5@gmail.com>
2022-04-27 16:25:45 -0500

    completed codewars solution, needed to research parseInt and why its important

--- 8kyu/Binary to Dec/main.js
+++ 8kyu/Binary to Dec/main.js
@@ -1,1 +1,15 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+function binToDec(bin){
+	let dec = parseInt(bin, 2)
+	return dec
+}
 
+console.log(binToDec(['1001001']))
+
+//top solution
+function binToDec(bin){
+	return parseInt(bin,2);
+  }
+
+  //other solution
+  const binToDec = bin => parseInt(bin,2);
