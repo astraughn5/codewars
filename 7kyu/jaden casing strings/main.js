3389e5982a6f8263c3b9e7ef5f41a80e19f2eabc
astraughn5 <astraughn5@gmail.com>
2022-05-05 13:07:36 -0500

    adding codewars, did not solve -- did not use this and .map()

--- 7kyu/jaden casing strings/main.js
+++ 7kyu/jaden casing strings/main.js
@@ -1,1 +1,33 @@
+// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
 
+// Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+// Example:
+
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+
+jadenCase = function (str) {
+	let splitter = str.split([' '])
+	newSplitter = splitter.map(word => {
+		return word.charAt(0).toUpperCase() + word.slice(1)
+	});
+	return newSplitter.join(' ');
+  };
+
+console.log(jadenCase("How can mirrors be real if our eyes aren't real"))
+
+//was not able to solve, was stuck using forEach when it shouldve been map , also using the prototype object threw me off and did not use this
+
+//solution
+String.prototype.toJadenCase = function () { 
+	return this.split(" ").map(function(word){
+	  return word.charAt(0).toUpperCase() + word.slice(1);
+	}).join(" ");
+  }
+
+  // other solution
+  String.prototype.toJadenCase = function() {
+	return this.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join(' ')
+  };
