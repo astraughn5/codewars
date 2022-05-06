a041e967abbbbf40dc1f915956b1da33d1cb6cdc
astraughn5 <astraughn5@gmail.com>
2022-05-06 12:42:54 -0500

    adding codewars, was able to solve

--- 8kyu/remove string spaces/main.js
+++ 8kyu/remove string spaces/main.js
@@ -1,1 +1,25 @@
+// Simple, remove the spaces from the string, then return the resultant string.
 
+
+// my solution
+function noSpace(x){
+	return x.split(' ').join('')
+  }
+
+
+
+// using reg ex
+function noSpace(x){
+	return x.replace(/\s/g, '');
+  }
+
+  // other solution 
+  function noSpace(x){
+	var result = ""
+	for(var index = 0; index < x.length; index++){
+	  if(x[index] !== " "){
+		result += x[index];
+	  }
+	}
+	return result;
+  }
