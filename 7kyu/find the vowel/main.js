56856612aa328c87b13d2a72387b29e6a22a2e7f
astraughn5 <astraughn5@gmail.com>
2022-05-09 11:34:26 -0500

    adding codewars, was unable to solve

--- 7kyu/find the vowel/main.js
+++ 7kyu/find the vowel/main.js
@@ -53,3 +53,16 @@ function vowelIndices(word){
   }
 
 console.log(vowelIndices('Apple'))
+
+
+// other solution
+function vowelIndices(word){
+	vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'];
+	result = []
+	for (var i = 0; i < word.length; i++) {
+	  if (vowels.indexOf(word[i]) != -1) {
+		result.push(i + 1);
+	  }
+	}
+	return result;
+  }
