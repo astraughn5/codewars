a1fe90476f78b788c1b4e57a6aa8fa449f86af05
astraughn5 <astraughn5@gmail.com>
2022-05-09 11:30:11 -0500

    adding codewars, was unable to solve

--- 7kyu/find the vowel/main.js
+++ 7kyu/find the vowel/main.js
@@ -10,3 +10,46 @@
 // NOTES
 // Vowels in this context refers to: a e i o u y (including upper case)
 // This is indexed from [1..n] (not zero indexed!)
+
+// tried solution with loop and could not get the zero index to stop
+function myVowelIndices(word){
+	let vowelArray = []
+	let lowerWord = word.toLowerCase().toString()
+	// let vowels = ['a', 'e', 'i', 'o', 'u']
+	for (let i = 0 ; i < lowerWord.length; i++ ){
+		// console.log(word[i])
+		if ( lowerWord[i] === 'a' || lowerWord[i] === 'e' || lowerWord[i] === 'i' || lowerWord[i] === 'o' || lowerWord[i] === 'u' || lowerWord[i] === 'y'){
+			let vowelIndex = word.indexOf(word[i])
+			console.log(word[i])
+			if (vowelIndex === -1){
+				vowelIndex = word.length
+				vowelArray.push(vowelIndex)
+			}
+			else {
+				vowelIndex = word.indexOf(word[i + 1])
+				vowelArray.push(vowelIndex)
+				console.log(vowelIndex)
+			}
+			}
+		else{
+		}
+	}
+	console.log(vowelArray)	
+}
+
+
+
+// solution
+function vowelIndices(word){
+	const str = 'aeiouy';
+	const arr = word.toLowerCase().split('');
+	let answer = [];
+	arr.forEach((name, i) => {
+	  if (str.indexOf(name) !== -1) {
+		answer.push(i + 1);
+	  }
+	});
+	return answer;
+  }
+
+console.log(vowelIndices('Apple'))
