556a4679845b58b840efd5d3537e3378bed64ef8
astraughn5 <astraughn5@gmail.com>
2022-05-31 16:25:00 -0500

    adding codewars, got the example solution but did not understand goal

--- 7kyu/dropWhile function/main.js
+++ 7kyu/dropWhile function/main.js
@@ -3,19 +3,32 @@
 function isEven(num) {
 	return num % 2 === 0;
   }
-  var seq = [2,4,6,8,1,2,5,4,3,2];
+  var seq = [2,4,6,8,1,2,5,4,3,2,1,3,3];
 
   function dropWhile(arr, pred) {
-  let data = []
-	console.log(arr)
-  console.log(arr.map(pred))
-	for (let i = 0; i <= arr.length; i++){
-		if (arr.map(pred) === 'true'){
-			data.push(i)
-		}
-	}
-	console.log(data)
-
-
+	for (var i = 0; i < arr.length && pred(arr[i]); i++) ;
+	console.log(arr.slice(i)) 
+	// console.log(arr.map(pred))
+// console.log(pred(arr.length))
+// if (pred(arr.length) === true){
+// 	//console.log(arr.slice(4))
+// 	console.log(arr.shift())
+// }
+// else {
+// 	return []
 }
+
 dropWhile(seq,isEven) // should return [1,2,5,4,3,2]
+
+
+//solution
+function dropWhile1(arr, pred) {
+	for (var i = 0; i < arr.length && pred(arr[i]); ++i) ;
+	return arr.slice(i)
+  }
+
+  //other solution
+  function dropWhile2(arr, pred) {
+	while (arr.length && pred(arr[0])) arr.shift()
+	return arr
+  }
